e7cfa15b3e8aa3a81457e0d80ee706a5fe851562
Anirudh Achal <anirudh.achal@gmail.com>
2020-08-02 15:15:35 +0530

    files renamed

--- VS/CP_Template/CP_Template/CP_Template.cpp
+++ VS/CP_Template/CP_Template/CP_Template.cpp
@@ -16,7 +16,7 @@
 
 #define all(x) x.begin(), x.end()
 #define debug(x) cout << #x << " = " << x << "\n"
-#define MOD 1000000007
+#define MOD 998244353
 
 const long double PI = 3.141592653589793236L;
 
@@ -25,8 +25,47 @@ typedef long double ld;
 
 using namespace std;
 
+const int maxN = 1e5;
+vector<int> adj[maxN + 1];
+vector<int> deg(maxN + 1);
+
 void solve()
 {
+    int n, m;
+
+    vector<pair<int, int>> E(m);
+
+    for (int i = 0; i < m; i++)
+    {
+        cin >> E[i].first >> E[i].second;
+        adj[E[i].first].push_back(E[i].second);
+        adj[E[i].second].push_back(E[i].first);
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        deg[i] = (int)adj[i].size();
+    }
+
+    ll sadness = 0;
+
+    for (int i = 0; i < m; i++)
+    {
+        int x = deg[E[i].first];
+        int y = deg[E[i].second];
+
+        sadness++;
+
+        sadness += n - x + n - y;
+
+        deg[E[i].first]--;
+        deg[E[i].second]--;
+
+        cout << sadness << "\n";
+
+    }
+
+
 
 }
 
@@ -36,7 +75,7 @@ int main()
     cin.tie(NULL);
 
     int T = 1;
-    cin >> T;
+    //cin >> T;
 
     while (T--)
     {
@@ -45,4 +84,6 @@ int main()
 
     return 0;
 }
+
+
 
